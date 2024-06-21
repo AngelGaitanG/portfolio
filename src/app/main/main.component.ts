cfa2ec7a8b9e33c52aceefb3770978562059a3db
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-06-21 14:53:26 -0300

    building the skeleton of the page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LeftComponent } from './components/left/left.component';
+import { RightComponent } from './components/right/right.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [LeftComponent, RightComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
