c0dfa932e803180a27c62cd5bd7cf870fa8c9cb7
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-06-22 16:36:12 -0300

    right skeleton finished

--- src/app/main/components/left/left.component.ts
+++ src/app/main/components/left/left.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-left',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './left.component.html',
   styleUrl: './left.component.css'
 })
 export class LeftComponent {
+  tecnologies = ["TypeScript", "NestJS", "PostgreSQL", "Github", "Git", "Angular", "JavaScript", "HTML", "CSS", "Node", "React", "mongoDB", "Express", "Docker",]
 
 }
