7f27505baf59b72a16ab46647a5d153700f49736
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-06-22 19:01:06 -0300

    building another views

---
+++ src/app/wid/wid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wid',
+  standalone: true,
+  imports: [],
+  templateUrl: './wid.component.html',
+  styleUrl: './wid.component.css'
+})
+export class WidComponent {
+
+}
