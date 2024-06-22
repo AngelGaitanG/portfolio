4b60f36d0423b70c3a8e9f568a34f6790b0941b2
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-06-22 18:27:17 -0300

    right skeleton finished

--- src/app/main/components/right/right.component.ts
+++ src/app/main/components/right/right.component.ts
@@ -1,12 +1,40 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-right',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './right.component.html',
   styleUrl: './right.component.css'
 })
 export class RightComponent {
+  projects = [
+    {
+      img: 'https://www.freecodecamp.org/espanol/news/content/images/size/w2000/2023/11/Thumbnail-Version-2.png',
+      title: 'Proyecto',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    },
+    {
+      img: 'https://www.freecodecamp.org/espanol/news/content/images/size/w2000/2023/11/Thumbnail-Version-2.png',
+      title: 'Proyecto2',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    },
+    {
+      img: 'https://www.freecodecamp.org/espanol/news/content/images/size/w2000/2023/11/Thumbnail-Version-2.png',
+      title: 'Proyecto3',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    },
+    {
+      img: 'https://www.freecodecamp.org/espanol/news/content/images/size/w2000/2023/11/Thumbnail-Version-2.png',
+      title: 'Proyecto4',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    },
+    {
+      img: 'https://www.freecodecamp.org/espanol/news/content/images/size/w2000/2023/11/Thumbnail-Version-2.png',
+      title: 'Proyecto5',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    }
+  ]
 
 }
