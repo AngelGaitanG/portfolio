605ba8a35aed9ca52a46e252dd9c3d5c16c795dc
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-06-22 22:12:43 -0300

    adding more views and about-me section skeleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { AboutComponent } from './about/about.component';
+import { WidComponent } from './wid/wid.component';
+import { WorksComponent } from './works/works.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'index', component: MainComponent
+    },
+    {
+        path: 'about', component: AboutComponent
+    },
+    {
+        path: 'what-i-do', component: WidComponent
+    },
+    {
+        path: 'works', component: WorksComponent
+    },
+    {
+        path: 'contact', component: ContactComponent
+    },
+    {
+        path: '404', component: NotFoundPageComponent
+    },
+    {
+        path: '',
+        redirectTo: 'index',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '404',
+        pathMatch: 'full'
+    }
+];
